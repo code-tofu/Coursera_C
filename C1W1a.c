f640830b392b38500c2269f2db8179eab2c16568
benfoo268 <benjamin.ftc@gmail.com>
2021-12-02 21:29:08 +0800

    initial commit

---
+++ C1W1a.c
@@ -0,0 +1,8 @@
+/* The following program is suppose to write hello world onto the screen but it has syntax errors  - find and correct.
+#include<stdio.h>
+int main(void)
+{   
+    printf(‘ hello world\n’);
+    return 0;
+}    
+ */
---
+++ C1W2b.c
@@ -0,0 +1,4 @@
+/*
+Write a program that can give the sine of a value between 0 and 1 (non inclusive).
+You will be graded based on whether the program can output a value in the correct range and whether your code is well-formatted and logically correct.
+*/
